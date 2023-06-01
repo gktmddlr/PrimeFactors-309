8e655bf0f734062d150c8775a48b766f1b861c4d
sssikha <gktmddlr@gmail.com>
2023-06-01 15:30:13 +0900

    [feature] 37p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,14 @@
 #include "pch.h"
 #include "../Project8/PrimeFactor.cpp"
 
-TEST(TestCaseName, TestName) {
+TEST(PrimeFactors, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
   EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
